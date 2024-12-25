c5784b515f0e1f0693322317522e88fa08a313b7
Jalay Movaliya <jalaymovaliya@Jalays-MacBook-Air.local>
2024-12-25 20:42:43 +0530

    / api

--- index.js
+++ index.js
@@ -62,6 +62,10 @@ io.on('connection', (socket) => {
     });
 });
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
 server.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
